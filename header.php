ab3a49ab36ee2246706ace4225f72a15663142db
Andre Ivan <andre801mz@gmail.com>
2015-11-23 14:51:32 -0430

    Agregar php

---
+++ header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Geekguin</title>
+
+    <!-- Material Design Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Material Design Bootstrap -->
+    <link href="../css/mdb.css" rel="stylesheet">
+
+    <!-- style -->
+    <link href="../css/style.css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../img/favicon.ico" type="img/favicon.icon" /> 
+</head>
+
+<body>
+    <!-- Nav -->
+    <nav class="navbar navbar-inverse z-depth-2 navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand waves-effect waves-light page-scroll" href="#home" style="padding-top: 0px;"><img src="../img/logo.png" height="193" width="170" alt="" class="img-responsive"></a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="#home" class="waves-effect waves-light page-scroll">Home</a></li>
+                    <li><a href="#about" class="waves-effect waves-light page-scroll">About</a></li>
+                    <li><a href="#services" class="waves-effect waves-light page-scroll">Services</a></li>
+                    <li><a href="#team" class="waves-effect waves-light page-scroll">Team</a></li>
+                    <!-- <li><a href="#portfolio" class="waves-effect waves-light page-scroll">Portafolio</a></li>
+                    <li><a href="#blog" class="waves-effect waves-light page-scroll">Blog</a></li> -->
+                    <li><a href="#contact" class="waves-effect waves-light page-scroll">Contact</a></li>
+                    <li class="dropdown">
+                            <a href="#" class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" role="button" aria-expanded="false">Idioma/Language <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="../index.php">Spanish/Español</a></li>
+                                <li><a href="en/index.php">English/Ingles</a></li>
+                            </ul>
+                        </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
